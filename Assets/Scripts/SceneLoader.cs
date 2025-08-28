acaee21f4a58db7f10ab8aab365f9fabee10aad0
Ragnarok1026 <cooperhurst1026@gmail.com>
2025-08-28 10:38:31 -0500

    hkgsdfhjkgdtfhjgkdf

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,27 @@
+using JetBrains.Annotations;
+using System.Collections;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+    public void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            LoadNextLevel();
+        }
+    }
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
