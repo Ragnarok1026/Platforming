68e77db79216ce6b237d9b60f3b7baa0673ee70c
Ragnarok1026 <cooperhurst1026@gmail.com>
2025-10-28 09:18:32 -0500

    Replace yield return null with WaitForEndOfFrame
    Updated coroutine loops in BossFight2 to use 'yield return new WaitForEndOfFrame()' instead of 'yield return null' for smoother frame timing during boss movement.

--- Assets/Scripts/BossFight2.cs
+++ Assets/Scripts/BossFight2.cs
@@ -31,21 +31,21 @@ public class BossFight2 : MonoBehaviour
             while (Vector2.Distance(transform.position, attackPointA.transform.position) > 1f)
             {
                 transform.Translate((attackPointA.transform.position - transform.position) * speed * Time.deltaTime);
-                yield return null;
+                yield return new WaitForEndOfFrame();
             }
             yield return new WaitForSeconds(3);
 
             while(Vector2.Distance(transform.position, attackPointB.transform.position) > 1f)
             {
                 transform.Translate((attackPointB.transform.position - transform.position) * speed * Time.deltaTime);
-                yield return null;
+                yield return new WaitForEndOfFrame();
             }
             yield return new WaitForSeconds(3);
 
             while(Vector2.Distance(transform.position, attackPointStart.transform.position) > 1f)
             {
                 transform.Translate((attackPointStart.transform.position - transform.position) * speed * Time.deltaTime);   
-                yield return null;
+                yield return new WaitForEndOfFrame();
             }
             yield return new WaitForSeconds(3);
         }
