0c955be9880655d533b6af6b4174483250c0dc75
Ragnarok1026 <cooperhurst1026@gmail.com>
2025-11-13 10:47:24 -0600

    Add new particle system prefab and update scenes
    Added 'Particle System' prefab and associated meta files in a new 'Prefab 1' folder. Introduced new scripts and sprites for Level 3, and replaced 'Level 3.unity' with 'Level 2.5.unity' in the scenes, updating build settings and related meta files. Also updated font assets and sprite metadata.

---
+++ Assets/Scripts/Level 3/Inverted Cubit.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using System.Collections;
+
+public class InvertedCubit : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem ParticleSystem;
+    private ParticleSystem ParticleSystem1;
+    public GameObject Player;
+    public GameObject Entity;
+    public GameObject Particle;
+    public GameObject Monolouge1;
+    public GameObject Monolouge2;
+    public GameObject Monolouge3;
+    public GameObject Monolouge4;
+    public Animator Animator;
+    public bool cutscene1 = false;
+    public bool cutscene2 = false;
+    public bool cutscene3 = false;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == Player)
+        {
+            Player.GetComponent<PlayerMovement>().enabled = false;
+            Player.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(0, 0);
+            Animator.SetFloat("Speed", 0);
+            Monolouge1.SetActive(true);
+            StartCoroutine(EntityMonolouge());
+        }
+    }
+    public IEnumerator EntityMonolouge()
+    {
+        while (cutscene1 == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Invoke("Cutscene1", 0);
+                cutscene1 = true;
+            }
+            yield return null;
+        }
+        while (cutscene2 == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && cutscene2 == false)
+            {
+                Invoke("Cutscene2", 0);
+                cutscene2 = true;
+            }
+            yield return null;
+        }
+        while (cutscene3 == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && cutscene3 == false)
+            {
+                Invoke("Cutscene3", 0);
+                cutscene3 = true;
+            }
+            yield return null;
+        }
+        while(cutscene3 == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Space) && cutscene3 == true)
+            {
+                Invoke("Creation1", 0);
+                cutscene3 = false;
+            }
+            yield return null;
+        }
+
+    }
+    void Cutscene1()
+    {
+        Monolouge1.SetActive(false);
+        Monolouge2.SetActive(true);
+    }
+    void Cutscene2()
+    {
+        Monolouge2.SetActive(false);
+        Monolouge3.SetActive(true);
+    }
+    void Cutscene3()
+    {
+        Monolouge3.SetActive(false);
+        Monolouge4.SetActive(true);
+    }
+    void Creation1()
+    {
+        Monolouge4.SetActive(false);
+        Entity.GetComponent<Rigidbody2D>().gravityScale = -1f;
+        Invoke("Creation2", 0.8f);
+    }
+    void Creation2()
+    {
+        Entity.GetComponent<Rigidbody2D>().gravityScale = 3f;
+        Invoke("SpawnParticles", 1);
+    }
+    void SpawnParticles()
+    {
+        ParticleSystem1 = Instantiate(ParticleSystem, Particle.transform.position, Quaternion.identity);
+    }
+}
