c28925638524110864a987671044858dac9f69e0
Ragnarok1026 <cooperhurst1026@gmail.com>
2025-12-08 08:37:54 -0600

    Add Arrow prefab and shield cutscene logic
    Introduced an Arrow prefab and related meta files. Updated Boss 3 scene to add ArrowSpawner objects, reorganize scene hierarchy, and implement shield cutscene logic via the new StartShadow script. Modified ShadowFight to remove unused coroutine. Adjusted EnergyShield sprite pivot and alignment. Added 'Shadow' tag to project settings.

--- Assets/Scripts/Level 3/ShadowFight.cs
+++ Assets/Scripts/Level 3/ShadowFight.cs
@@ -19,17 +19,6 @@ public class ShadowFight : MonoBehaviour
         {
             Boss.GetComponent<Rigidbody2D>().gravityScale = -1;
             fightStarted = true;
-            StartCoroutine(Fight());
         }
-    }
-    IEnumerator Fight()
-    {
-        yield return new WaitForSeconds(0.8f);
-        while(fightStarted == true)
-        {
-            Boss.GetComponent<Rigidbody2D>().gravityScale = 0;
-            yield return null;
-        }
-        yield return null;
     }
 }
---
+++ Assets/Scripts/Level 3/StartShadow.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class StartShadow : MonoBehaviour
+{
+    public GameObject Boss;
+    public GameObject Shield;
+    public float shieldSpeed = 0.01f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shield.transform.Rotate(Vector3.forward * shieldSpeed);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Shadow")
+        {
+            Debug.Log("Start Cutscene");
+            Boss.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+            Shield.SetActive(true);
+        }
+    }
+}
