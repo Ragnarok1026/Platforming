bad180915354b1ec5cea4d06d92adcbe106b7ad4
Ragnarok1026 <cooperhurst1026@gmail.com>
2026-01-06 10:44:54 -0600

    Refactor boss animations and update BossHealth logic
    Moved boss animation assets to a new Animations folder and expanded the Boss.controller with additional states and transitions. Updated BossHealth.cs to support new animation triggers, references to new stop/target GameObjects, and improved death handling logic. Added new animation and sprite assets for boss death, hurt, and purification sequences. Updated the Boss 1 scene to use new references, added new GameObjects for movement/targeting, and adjusted collider and transform properties for improved boss behavior.

--- Assets/Scripts/Level 1/BossHealth.cs
+++ Assets/Scripts/Level 1/BossHealth.cs
@@ -4,6 +4,15 @@ public class BossHealth : MonoBehaviour
 {
     public int maxHealth = 20;
     public int currentHealth;
+    public int speed = 8;
+    public Collider2D bossCollider;
+    public Animator animator;
+    public GameObject oldLeft;
+    public GameObject oldRight;
+    public GameObject newLeft;
+    public GameObject newRight;
+    public GameObject target;
+    public GameObject newTarget;
     void Start()
     {
         currentHealth = maxHealth;
@@ -14,14 +23,37 @@ public class BossHealth : MonoBehaviour
     }
     public void TakeDamage(int damage)
     {
+        animator.SetTrigger("Hurt");
         currentHealth -= damage;
         if (currentHealth <= 0)
         {
+            animator.SetBool("isDead", true);
             Die();
+        }
+        else
+        {
+            animator.SetBool("isDead", false);
         }
     }
     void Die()
     {
-        Destroy(gameObject);
+        Destroy(bossCollider);
+        if (transform.position.x <= oldLeft.transform.position.x)
+        {
+            speed = -8;
+        }
+        else if (transform.position.x >= oldRight.transform.position.x)
+        {
+            speed = 8;
+        }
+        oldLeft.transform.position = newLeft.transform.position;
+        oldRight.transform.position = newRight.transform.position;
+        GetComponent<BossGoUp>().Speed = 2;
+        Invoke("BossDeath", 2);
+    }
+    void BossDeath()
+    {
+        GetComponent<BossGoUp>().Speed = 2;
+        target.transform.position = newTarget.transform.position;
     }
 }
