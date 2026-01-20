d8b408f3342865d0176c02846dc918a6b0fa4c6b
Ragnarok1026 <cooperhurst1026@gmail.com>
2026-01-20 10:49:08 -0600

    Organize scenes into subfolders and add main menu
    Moved boss, level, and menu-related scenes into dedicated subfolders for better project organization. Added new MainMenu and Settings scenes, and updated EditorBuildSettings to reflect the new structure. Also made a minor sorting order adjustment in Level 1 and added a Buttons script for menu controls.

---
+++ Assets/Scripts/Level 1/Buttons.cs
@@ -0,0 +1,35 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+}
