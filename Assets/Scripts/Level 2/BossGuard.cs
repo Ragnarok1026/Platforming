f00e86a790e1b1099f9e7631bea632eefad26ae4
Ragnarok1026 <cooperhurst1026@gmail.com>
2026-01-23 10:45:56 -0600

    Update Boss 2 scene with new wave and trigger objects
    Reorganized several GameObjects in the Boss 2 scene, including replacing and repositioning 'Wave', 'Handle', and 'Tip' objects, updating their sprites and colors, and adjusting hierarchy and transforms. Restored and updated 'Virus' and 'Trigger' objects, and re-enabled the 'Warning' object with its animator. These changes likely support new boss mechanics or visual effects.

--- Assets/Scripts/Level 2/BossGuard.cs
+++ Assets/Scripts/Level 2/BossGuard.cs
@@ -12,6 +12,8 @@ public class BossGuard : MonoBehaviour
     public GameObject vines2;
     public GameObject stop1;
     public GameObject stop2;
+    public GameObject startBoss;
+    public GameObject Stage1;
     public float speed = 150f;
     void Start()
     {
@@ -21,7 +23,7 @@ public class BossGuard : MonoBehaviour
     {
         if (start == null)
         {
-            transform.position = Vector3.MoveTowards(transform.position, hover.transform.position, 5f * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, hover.transform.position, 10f * Time.deltaTime);
         }
         if(Vector3.Distance(transform.position, hover.transform.position) < 0.1f)
         {
@@ -32,14 +34,17 @@ public class BossGuard : MonoBehaviour
     }
     IEnumerator Warning()
     {
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(0f);
         warning1.SetActive(true);
         warning2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        warning1.GetComponent<SpriteRenderer>().enabled = false;
+        warning2.GetComponent<SpriteRenderer>().enabled = false;
+        vines1.transform.position = Vector3.MoveTowards(vines1.transform.position, stop1.transform.position, 10f * Time.deltaTime);
+        vines2.transform.position = Vector3.MoveTowards(vines2.transform.position, stop2.transform.position, 10f * Time.deltaTime);
         yield return new WaitForSeconds(2f);
-        warning1.SetActive(false);
-        warning2.SetActive(false);
-        vines1.transform.position = Vector3.MoveTowards(vines1.transform.position, stop1.transform.position, 3f * Time.deltaTime);
-        vines2.transform.position = Vector3.MoveTowards(vines2.transform.position, stop2.transform.position, 3f * Time.deltaTime);
+        Destroy(startBoss);
+        Stage1.SetActive(true);
         yield return null;
     }
 }
